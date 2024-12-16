f0e96f0ab21be58de2974dbbefbd47a5ab28d239
Kamoliddin <108530207+ravshanovdev@users.noreply.github.com>
2024-12-16 15:25:40 +0500

    class based handlers darsini tamomladim

--- app.py
+++ app.py
@@ -1,17 +1,53 @@
 from webob import Request, Response
+from parse import parse
+import inspect
 
 
 class PySauronApp:
+    def __init__(self):
+        self.routes = dict()
+
     def __call__(self, environ, start_response):
         request = Request(environ)
         response = self.handle_request(request)
         return response(environ, start_response)
 
     def handle_request(self, request):
-
-        user_agent = request.environ.get("HTTP_USER_AGENT", "user agent not found")
-
         response = Response()
-        response.text = f"hello my friend: {user_agent}"
+
+        handler, kwargs = self.find_handler(request)
+        if handler is not None:
+            if inspect.isclass(handler):
+                handler = getattr(handler(), request.method.lower(), None)
+
+                if handler is None:
+                    response.status_code = 405
+                    response.text = "Method not allowed"
+                    return response
+
+            handler(request, response, **kwargs)
+        else:
+            self.default_response(response)
 
         return response
+
+    def find_handler(self, request):
+        for path, handler in self.routes.items():
+            parsed_result = parse(path, request.path)
+
+            if parsed_result is not None:
+                return handler, parsed_result.named
+        return None, None
+
+    def default_response(self, response):
+        response.status_code = 404
+        response.text = 'Not Found'
+
+    def route(self, path):
+        assert path not in self.routes, "Duplicate Rout. Please Change The URL"
+
+        def wrapper(handler):
+            self.routes[path] = handler
+            return handler
+
+        return wrapper
--- main.py
+++ main.py
@@ -2,3 +2,27 @@ from app import PySauronApp
 
 app = PySauronApp()
 
+
+@app.route("/home")
+def home(request, response):
+    response.text = "hi this is home page"
+
+
+@app.route("/about")
+def about(request, response):
+    response.text = 'hi this is about page'
+
+
+@app.route("/hello/{name}")
+def generating(request, response, name):
+    response.text = f"hello {name}"
+
+
+@app.route("/book")
+class Books:
+    def get(self, request, response):
+        response.text = "this is get method"
+
+    def post(self, request, response):
+        response.text = "endpoint to create a book"
+
