a20c93cb33dd1bb759a76aa65d17ac80e853a0ea
Kamoliddin <108530207+ravshanovdev@users.noreply.github.com>
2025-03-17 19:33:14 +0500

    added requirements file

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 from pylord.app import PyLordApp
 from pylord.middleware import Middleware
+from pylord.orm import ForeignKey, Table, Column, Database
+import threading
+
+threading_local = threading.local()
+
+
+def get_db():
+    if not hasattr(threading_local, "db"):
+        threading_local.db = Database("./test_main.db")
+    return threading_local.db
+
 
 app = PyLordApp()
 
@@ -75,10 +86,73 @@ def json_handler(req, resp):
     resp.json = response_data
 
 
+class Category(Table):
+    name = Column(str)
 
 
+class Blog(Table):
+    category = ForeignKey(Category)
+    name = Column(str)
+    description = Column(str)
 
 
+# db.create(Category)
+# db.create(Blog)
 
 
+@app.route("/create_category", allowed_methods=["post"])
+def create_category(req, resp):
+    db = get_db()
+    db.create(Category)
+    category = Category(**req.POST)
+    db.save(category)
+
+    resp.status_code = 201
+    resp.json = {"name": category.name}
+
+
+@app.route("/get_all_category", allowed_methods=["get"])
+def get_all_category(req, resp):
+    db = get_db()
+    category = db.all(Category)
+
+    resp.status_code = 200
+    resp.json = [{"category_id": cat.id, "category_name": cat.name} for cat in category]
+
+
+@app.route("/create_blog", allowed_methods=['post'])
+def crete_blog(req, resp):
+    db = get_db()
+    db.create(Blog)
+    data = req.json
+    category = db.get(Category, data["category"])
+    if not category:
+        resp.status_code = 404
+        resp.json = {"error": "Category Not Found"}
+        return
+
+    blog = Blog(
+        category=category,
+        name=data["name"],
+        description=['description'],
+
+    )
+
+    resp.status_code = 201
+    resp.json = {
+        "blog_id": blog.id,
+        "blog_category": str(blog.category),
+        "blog_name": blog.name,
+        "blog_description": blog.description
+
+    }
+
+
+@app.route("/all_blog", allowed_methods=['get'])
+def get_all_blog(req, resp):
+    db = get_db()
+    blogs = db.all(Blog)
+
+    resp.status_code = 200
+    resp.json = [{"blog_id": blog.id, "blog_category": blog.category, "blog_name": blog.name, "blog_description": blog.description} for blog in blogs]
 
--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/me/myproject'
 EMAIL = 'kravshanov97@gmail.com'
 AUTHOR = 'Kamoliddin Ravshanov'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.1.2'
+VERSION = '0.2.0'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
